e448c06b92d43654450b255336bf4a1874da4356
Stephen Pryor <stpryor@microsoft.com>
2018-03-08 14:16:59 -0800

    Remove old breadcrumbs component

--- src/components/pages/maintenance/jobDetails/jobDetails.js
+++ src/components/pages/maintenance/jobDetails/jobDetails.js
@@ -2,8 +2,6 @@
 
 import React from 'react';
 
-export const Breadcrumbs = ({ match: { params: { id } } }) => `Maintenance > ${id}`;
-
 export const JobDetails = ({ match: { params: { id } } }) => (
   <div className="maintenance-container">
     <div className="header">{id}</div>
