21b5483ac96f6ef30ecadc20f21c83c720b39847
Stephen Pryor <stpryor@microsoft.com>
2018-03-08 14:15:03 -0800

    Fix some styling issues

--- src/components/pages/maintenance/ruleDetails/ruleDetails.js
+++ src/components/pages/maintenance/ruleDetails/ruleDetails.js
@@ -2,8 +2,6 @@
 
 import React from 'react';
 
-export const Breadcrumbs = ({ match: { params: { id } } }) => `Maintenance > ${id}`;
-
 export const RuleDetails = ({ match: { params: { id } } }) => (
   <div className="maintenance-container">
     <div className="header">{id}</div>
