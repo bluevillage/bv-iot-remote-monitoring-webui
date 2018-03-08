1b54a31dad10ff43168a9571f00021a4db89d214
Stephen Pryor <stpryor@microsoft.com>
2018-03-08 14:06:35 -0800

    Add breadcrumbs and settings flyout

--- src/components/pages/maintenance/jobDetails/jobDetails.js
+++ src/components/pages/maintenance/jobDetails/jobDetails.js
@@ -1,11 +1,11 @@
 // Copyright (c) Microsoft. All rights reserved.
 
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+
+export const Breadcrumbs = ({ match: { params: { id } } }) => `Maintenance > ${id}`;
 
 export const JobDetails = ({ match: { params: { id } } }) => (
   <div className="maintenance-container">
-    <NavLink to={'/maintenance/jobs'}>Back</NavLink>
     <div className="header">{id}</div>
   </div>
 );
--- src/components/pages/maintenance/ruleDetails/ruleDetails.js
+++ src/components/pages/maintenance/ruleDetails/ruleDetails.js
@@ -1,11 +1,11 @@
 // Copyright (c) Microsoft. All rights reserved.
 
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+
+export const Breadcrumbs = ({ match: { params: { id } } }) => `Maintenance > ${id}`;
 
 export const RuleDetails = ({ match: { params: { id } } }) => (
   <div className="maintenance-container">
-    <NavLink to={'/maintenance/notifications'}>Back</NavLink>
     <div className="header">{id}</div>
   </div>
 );
