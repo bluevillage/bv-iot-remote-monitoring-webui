d92976e640d2cd04ed797e149b11f9caa2a6d4bd
mechaffin <a-machaf>
2018-05-02 10:44:05 -0700

    change timerender to show max date as blank

--- src/components/shared/cellRenderers/timeRenderer/timeRenderer.js
+++ src/components/shared/cellRenderers/timeRenderer/timeRenderer.js
@@ -1,12 +1,18 @@
 // Copyright (c) Microsoft. All rights reserved.
 
 import moment from 'moment';
-import { DEFAULT_TIME_FORMAT, EMPTY_FIELD_VAL, gridValueFormatters } from 'components/shared/pcsGrid/pcsGridConfig';
+import {
+  DEFAULT_TIME_FORMAT,
+  EMPTY_FIELD_VAL,
+  MAX_DATE_STRING,
+  gridValueFormatters
+} from 'components/shared/pcsGrid/pcsGridConfig';
 
 const { checkForEmpty } = gridValueFormatters;
 
 const formatTime = (value) => {
   if (value) {
+    if (value === MAX_DATE_STRING) return '';
     const time = moment.utc(value).local();
     return checkForEmpty((time.unix() > 0) ? time.format(DEFAULT_TIME_FORMAT) : '');
   }
--- src/components/shared/pcsGrid/pcsGridConfig.js
+++ src/components/shared/pcsGrid/pcsGridConfig.js
@@ -7,6 +7,9 @@ export const EMPTY_FIELD_VAL = '---';
 /** The default formatting for dates in the PcsGrid */
 export const DEFAULT_TIME_FORMAT = 'hh:mm:ss A MM.DD.YYYY';
 
+/** The string representing an unknown future date (also, max date) */
+export const MAX_DATE_STRING = '9999-12-30T23:59:59.9999999Z';
+
 /** A collection of reusable value formatter methods */
 export const gridValueFormatters = {
   checkForEmpty: (value, emptyValue = EMPTY_FIELD_VAL) => value || emptyValue
