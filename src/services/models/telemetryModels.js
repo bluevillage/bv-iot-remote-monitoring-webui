b1a5dde1d87c6bec02cb1efa2ae7e17e1a25d7f4
Diane Hu <diane.hu@duke.edu>
2018-06-08 14:52:27 -0700

    Made action template match json format telemetry will expect

--- src/services/models/telemetryModels.js
+++ src/services/models/telemetryModels.js
@@ -113,7 +113,7 @@ export const toNewRuleRequestModel = ({
   const Actions = actions.map(act =>({
     ActionType: act.actionItem,
     Value: act.actionItem === "Email" ? act.emailAddresses : act.smsNumbers,
-    ActionTemplate: act.actionItem === "Email" ? act.emailTemplate : act.smsTemplate
+    ActionTemplate: {"TemplateString": act.actionItem === "Email" ? act.emailTemplate : act.smsTemplate}
   }))
   return {
     Name: name,
