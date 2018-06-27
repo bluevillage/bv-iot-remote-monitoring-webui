2d65f860c1c24a1657a64778599fbb188a3c9b93
Diane Hu <t-dih@microsoft.com>
2018-06-27 15:57:07 -0700

    Made slight design changes per Garth's redesign

--- src/components/shared/forms/pill.js
+++ src/components/shared/forms/pill.js
@@ -2,13 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { Svg } from 'components/shared/svg/svg';
-import { joinClasses } from 'utilities';
 
 import './styles/pill.css';
 
 export const Pill = ({ svg, label, onSvgClick}) => {
   return (
-    <div className={joinClasses('pill', 'pill-primary')} onClick={onSvgClick}>
+    <div className="pill" onClick={onSvgClick}>
       { label }
       { svg && <Svg path={svg} className="pill-icon" /> }
     </div>
